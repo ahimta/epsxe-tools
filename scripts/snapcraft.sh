5c6870ee0a7fc91bdfdb05a766a74b48c200a51e
Abdullah Alansari <ahimta@gmail.com>
2019-12-14 16:36:34 +0300

    feat: add unrestricted Snap package:smiley:
    Everything seems to work fine except for sound which may be hard to get
    working without making changes to ePSXe itself and it's closed-source
    and only binaries are available:sweat_smile:.

---
+++ scripts/snapcraft.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+# This is a wrapper for `snapcraft` to make it easier to develop, test, and
+# release without any conflict with the local installation.
+# Usage: ./scripts/snapcraft.sh <variant> [snpacraft-arg...]
+
+# shellcheck disable=SC1091
+source ./scripts/_helpers.sh
+
+Variant=$1
+Name=$(get_snap_name "$Variant")
+
+case  $Variant  in
+"debug")
+  Title="ePSXe (Debug)"
+  Grade="devel"
+  ;;
+"release")
+  Title="ePSXe (Release)"
+  Grade="stable"
+  ;;
+"store")
+  Title="ePSXe"
+  Grade="stable"
+  ;;
+"help"|"--help"|*)
+  echo "Usage: ./scripts/snapcraft.sh <variant> [snpacraft-arg...]"
+  exit 0
+  ;;
+esac
+
+AllArgs=("$@")
+RestArgs=("${AllArgs[@]:1}")
+SnapcraftArgs=("${RestArgs[@]}")
+
+SnapcraftFile=snap/snapcraft.yaml
+SnapcraftTemplate=snap/local/snapcraft.scaffold.yaml
+
+VersionMajor="2"
+VersionMinor="0"
+VersionPatch="5"
+VersionGit=$(git rev-parse --short HEAD || echo "blablabla")
+Version="$VersionMajor.$VersionMinor.$VersionPatch+git-$VersionGit"
+
+rm --force ./*.snap
+rm --force $SnapcraftFile
+
+cp $SnapcraftTemplate $SnapcraftFile
+
+GenerationNote="# NOTE: This file is generated using 'scripts/snapcraft.sh'."
+sed --in-place "1s|^|$GenerationNote\n|" $SnapcraftFile
+
+sed --in-place "s|@@TITLE@@|$Title|g" $SnapcraftFile
+sed --in-place "s|@@NAME@@|$Name|g" $SnapcraftFile
+sed --in-place "s|@@GRADE@@|$Grade|g" $SnapcraftFile
+sed --in-place "s|@@VERSION@@|$Version|g" $SnapcraftFile
+
+snapcraft "${SnapcraftArgs[@]}"
